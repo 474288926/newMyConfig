5b80bee9b2fe8c5614b06825317535df7e522f84
yang <474288926@qq.com>
2023-12-13 21:06:30 +0800

    build:路由安装配置、声明type类型

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router/index";
+const app = createApp(App);
+app.use(router).mount("#app");
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+declare module "@/views/home/index.vue" {
+  import Vue from "vue";
+  export default Vue;
+}
+declare module "@/router/index" {
+  import VueRouter from "vue-router";
+  const router: VueRouter;
+  export default router;
+}
