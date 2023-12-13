5202f1bace4bcc81cfcf003870355033f4b1293f
yang <474288926@qq.com>
2023-12-13 22:40:01 +0800

    build:引入vueuse

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,6 +3,7 @@
     <header>home123131</header>
     <main>
       <p>Count: {{ counter.count }}</p>
+      -- {{ x }} -- {{ y }}
       <button @click="counter.increment">Increment</button>
       <button @click="counter.decrement">Decrement</button>
     </main>
@@ -12,6 +13,9 @@
 <script setup lang="ts">
 import { useCounterStore } from "@/store/counter/index";
 import { inject } from "vue";
+import { useMouse } from "@vueuse/core";
+// "x" and "y" are refs
+const { x, y } = useMouse();
 const counter = useCounterStore();
 const router = inject("router");
 console.log("router:", router);
