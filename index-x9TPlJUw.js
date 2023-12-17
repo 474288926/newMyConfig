14723d40902f1f5e52c6e60a0534b746a759af05
474288926 <474288926@users.noreply.github.com>
2023-12-17 08:06:25 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter b7a251064e3e3eb57c563ad51c0df09f2475f8b7

---
+++ index-x9TPlJUw.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as e,F as n,r,a as l,t}from"./index--RPAjovN.js";const o=s({},[["render",function(s,o){return a(),e("div",null,[(a(),e(n,null,r(1e3,(s=>l("div",{key:s,class:"p-4 m-2 bg-blue-300"},t(s),1))),64))])}]]);export{o as default};
