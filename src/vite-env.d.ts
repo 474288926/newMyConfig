daa40e7015e2c628efae7cbc1ebbdb8c17bd8d93
yang <474288926@qq.com>
2023-12-22 17:11:00 +0800

    chore: 声明验证码插件

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module 'vue-verify-code' {
+  const VueVerifyCode: any
+  export default VueVerifyCode
+}
+// 声明 importAll.js 模块的类型
+declare module '@/components/importAll' {
+  // 导入的模块类型
+  import { App } from 'vue'
+
+  // importAll 函数的类型声明
+  // eslint-disable-next-line no-unused-vars
+  export default function importAll(app: App): void
+}
