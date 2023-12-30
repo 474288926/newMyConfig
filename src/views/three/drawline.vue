4bf3a64d4db51c32565ecf88cd46df651a4df56b
yang <474288926@qq.com>
2023-12-30 22:47:04 +0800

    test: 测试pwa

--- src/views/three/drawline.vue
+++ src/views/three/drawline.vue
@@ -28,7 +28,7 @@ onMounted(() => {
   const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
   const points: any = []
   points.push(new THREE.Vector3(-10, 0, 0))
-  points.push(new THREE.Vector3(0, 10, 60))
+  points.push(new THREE.Vector3(0, 40, 60))
   points.push(new THREE.Vector3(10, 0, 0))
   const geometry = new THREE.BufferGeometry().setFromPoints(points)
   const line = new THREE.Line(geometry, material)
