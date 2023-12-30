1e5a7e5d00cf79c7e8a59f3ed30b31bd58fc5922
yang <474288926@qq.com>
2023-12-30 16:43:53 +0800

    build: pwa配置

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import VueVerifyCode from 'vue-verify-code'
 import Cookies from 'js-cookie'
+import { registerSW } from 'virtual:pwa-register'
 import App from '@/App.vue'
 import router from '@/router/index'
 import pinia from '@/store/index'
@@ -10,6 +11,10 @@ import '@/assets/css/index.css'
 import 'element-plus/dist/index.css'
 import importAll from '@/components/importAll'
 
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {}
+})
 const app = createApp(App)
 app.component('VueVerifyCode', VueVerifyCode)
 app.provide('router', router)
@@ -22,3 +27,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 app.use(ElementPlus)
 
 app.use(router).use(pinia).use(importAll).mount('#app')
+updateSW()
