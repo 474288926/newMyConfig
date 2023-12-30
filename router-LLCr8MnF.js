15dbe355a761f4640e53f596828e027987ad774a
474288926 <474288926@users.noreply.github.com>
2023-12-30 09:12:04 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 5864c7dcbfa246ddea1f8659bfd2259bbf139d3c

--- router-LLCr8MnF.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,e as s,c as e,f as t}from"./index-e3UwZUS2.js";const o={},a={class:"grid grid-cols-1 h-full"},i=[t("iframe",{src:"https://router.vuejs.org/zh/",width:"100%",height:"100%",frameborder:"0"},null,-1)];const d=r(o,[["render",function(r,t){return e(),s("div",a,i)}]]);export{d as default};
