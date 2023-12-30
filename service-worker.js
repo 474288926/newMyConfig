814e2b5cc75a38479bbf8f79c728fcbdc6afa25c
yang <474288926@qq.com>
2023-12-31 02:02:47 +0800

    test: pwa测试

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+// service-worker.js
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.action === 'skipWaiting') {
+    // eslint-disable-next-line no-restricted-globals
+    self.skipWaiting() // 立即激活新的 Service Worker
+  }
+})
+
+// 其他缓存策略和生命周期事件的处理
