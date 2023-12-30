88e8c2ed7554faa98219fb84016c7ae964c012dd
474288926 <474288926@users.noreply.github.com>
2023-12-30 17:46:21 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 626a831d9ccd7700e0d22dcee8776b9cc5e9c2a1

--- drawline-ZRYoneMI.js
+++ drawline-ZRYoneMI.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-F1ZPMo7T.js";
+import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-HBVLMcDJ.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
