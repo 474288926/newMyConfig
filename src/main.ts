a6f0c46eea1fb6765fceb37f8315c0edd006a732
yang <474288926@qq.com>
2023-12-31 07:57:01 +0800

    test: pwa测试

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ import '@/assets/css/index.css'
 import 'element-plus/dist/index.css'
 import importAll from '@/components/importAll'
 
-const app: any = createApp(App)
 registerSW({
   onNeedRefresh() {
     console.log('检测到新版本')
@@ -22,34 +21,14 @@ registerSW({
     console.log('注册sw成功')
     console.log('Service Worker 脚本 URL:', swScriptUrl)
     console.log('Service Worker 注册信息:', registration)
-    // 检测是否有新版本
-    registration
-      .update()
-      .then(() => {
-        console.log('Service Worker 更新检查完成')
-      })
-      .catch((error: any) => {
-        console.error('Service Worker 更新检查失败:', error)
-      })
   },
   onRegisterError() {
     console.log('注册sw失败')
   },
   immediate: true
 })
-// 在应用启动时调用初始化函数
-async function initializeServiceWorker() {
-  const registration: any = await navigator.serviceWorker.getRegistration()
-  if (registration) {
-    console.log('initialize', registration)
-    registration.active.postMessage({ action: 'initialize' })
-  }
-}
-
+const app: any = createApp(App)
 // 在应用程序挂载之前调用初始化函数
-app.beforeMount(() => {
-  initializeServiceWorker()
-})
 app.provide('router', router)
 app.provide('Cookies', Cookies)
 // eslint-disable-next-line no-restricted-syntax
