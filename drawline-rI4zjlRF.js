7cb2698220921b3019be10e9de3f0ff01de8df2e
474288926 <474288926@users.noreply.github.com>
2023-12-30 18:04:05 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 814e2b5cc75a38479bbf8f79c728fcbdc6afa25c

--- drawline-rI4zjlRF.js
+++ drawline-rI4zjlRF.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-HBVLMcDJ.js";
+import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-IL-q7Lm7.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
