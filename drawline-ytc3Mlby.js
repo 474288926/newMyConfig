a105494a8beb8f88809c37e73f721a20d0856628
474288926 <474288926@users.noreply.github.com>
2024-01-01 13:12:29 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter c185d6c439c4c7276f02605928ae98b90a3f83fd

--- drawline-ytc3Mlby.js
+++ drawline-ytc3Mlby.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-ifDtTkrQ.js";
+import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-IuckJ5z7.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
