f923408c8e55894d0a9c6fa3d1408785bf7d7ea6
474288926 <474288926@users.noreply.github.com>
2024-01-05 08:42:02 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 1b72088d92d91857f7c01803782e8c66fa9d9933

--- drawline-R8peZAga.js
+++ drawline-R8peZAga.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-gZzXnqfx.js";
+import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-oAjMBXcI.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
