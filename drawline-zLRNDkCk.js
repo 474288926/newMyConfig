64a89756adcbc7e374a0d9a391e3ff510c555c27
474288926 <474288926@users.noreply.github.com>
2024-01-06 04:55:13 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter f47fc9b77547edb5a54e1ad000fe6fc4dd8f0e27

--- drawline-zLRNDkCk.js
+++ drawline-zLRNDkCk.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-NEOXw6DN.js";
+import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-fyGUNt7N.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
