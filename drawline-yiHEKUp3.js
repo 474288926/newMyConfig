0f9cefeb929d338ebd4f384c41b294e5c4fc009b
474288926 <474288926@users.noreply.github.com>
2024-01-07 15:50:47 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter e0c1ddfcc4dd14b97c3b2f75f14d09295d2aa064

--- drawline-yiHEKUp3.js
+++ drawline-yiHEKUp3.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-cJkUGTti.js";
+import{d as t,r as e,o as n,c as i,e as r,_ as a}from"./index-VPeSIIdm.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
