8dd85441b50acce183fa5218a27db03295f2f675
yang <474288926@qq.com>
2024-01-08 03:53:29 +0800

    style: 退出登陆hover颜色调整

--- src/layouts/YHeader.vue
+++ src/layouts/YHeader.vue
@@ -39,7 +39,7 @@
       </template>
       <div class="py-1">
         <div
-          class="cursor-pointer flex items-center px-4 gap-2 py-2 hover:bg-blue-50 hover:text-blue-700 dark:hover:bg-blue-400/10"
+          class="cursor-pointer flex items-center px-4 gap-2 py-2 hover:bg-blue-50 hover:text-blue-400 dark:hover:text-blue-600 dark:hover:bg-blue-400/10"
           @click="onQuit"
         >
           <el-icon><Right /></el-icon>
