af7ae504c04657ae6e06750b37f620cef91a2a3b
yang <474288926@qq.com>
2024-01-13 01:37:25 +0800

    build: vite配置调整

--- src/components/public/GlobalLoading.vue
+++ src/components/public/GlobalLoading.vue
@@ -2,7 +2,7 @@
   <div class="app-loading">
     <div class="app-loading-wrap">
       <div class="app-loading-title">
-        <img src="/newMyConfig/vite.svg" class="app-loading-logo" alt="Logo" />
+        <img src="/vite.svg" class="app-loading-logo" alt="Logo" />
         <div class="app-loading-title">loading...</div>
       </div>
       <div class="app-loading-item">
