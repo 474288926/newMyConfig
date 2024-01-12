48d8f019b3659765f872db4808176a02468a24d9
yang <474288926@qq.com>
2024-01-13 00:17:47 +0800

    build: 调整打包设置

--- vite.config.ts
+++ vite.config.ts
@@ -144,7 +144,7 @@ export default defineConfig({
       }
     }
   },
-  base: '/newMyConfig/', // 设置打包路径
+  base: '/', // 设置打包路径
   build: {
     assetsDir: './',
     // 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
