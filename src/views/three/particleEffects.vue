b8c56660f79da32a451755f96880bfb372029789
yang <474288926@qq.com>
2024-01-19 09:26:55 +0800

    refactor: 粒子位置调整

--- src/views/three/particleEffects.vue
+++ src/views/three/particleEffects.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex-1 overflow-hidden">
     <div class="absolute top-0 left-0 flex gap-8 flex-col">
-      <div class="cursor-pointer" @click="updateGeometry(boxGeo, 100)">方形</div>
-      <div class="cursor-pointer" @click="updateGeometry(sphereGeo, -100)">圆形</div>
+      <div class="cursor-pointer" @click="updateGeometry(boxGeo, 80)">方形</div>
+      <div class="cursor-pointer" @click="updateGeometry(sphereGeo, -80)">圆形</div>
     </div>
     <div ref="canvasRef" class="h-full"></div>
   </div>
@@ -19,7 +19,7 @@ const canvasRef = ref<HTMLElement | null>(null)
 
 const scene = new THREE.Scene()
 scene.fog = new THREE.FogExp2(0x05050c, 0.0005)
-const camera = new THREE.PerspectiveCamera(60, 1, 10, 10000)
+const camera = new THREE.PerspectiveCamera(60, 1, 100, 10000)
 camera.position.set(90, 140, 200)
 const renderer = new THREE.WebGLRenderer()
 renderer.setPixelRatio(window.devicePixelRatio)
@@ -27,75 +27,90 @@ renderer.setPixelRatio(window.devicePixelRatio)
 // scene.add(axesHelper)
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const MAX = 15000
+const MAX = 1000
 const geo: any = new THREE.BufferGeometry()
-
+const aroundGeo = new THREE.BufferGeometry()
 const positions = new Float32Array(MAX * 3)
-
+const aroundPositions = new Float32Array(200 * 3)
+for (let i = 0, s = MAX, j = 0; i < s; i += 1, j += 3) {
+  positions[j] = Math.random() * 800 - 400
+  positions[j + 1] = Math.random() * 800 - 400
+  positions[j + 2] = Math.random() * 800 - 400
+  if (i < 200) {
+    aroundPositions[j] = Math.random() * 800 - 400
+    aroundPositions[j + 1] = Math.random() * 800 - 400
+    aroundPositions[j + 2] = Math.random() * 800 - 400
+  }
+}
 geo.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-
+aroundGeo.setAttribute('position', new THREE.BufferAttribute(aroundPositions, 3))
 const textureLoader = new THREE.TextureLoader()
 const pointMap = textureLoader.load(`${PUBLIC_DIR}three/gradient.png`)
+pointMap.colorSpace = THREE.SRGBColorSpace
 
 const mat = new THREE.PointsMaterial({
   size: 4,
   transparent: true,
   map: pointMap
 })
+const aroundMat = new THREE.PointsMaterial({
+  size: 2,
+  transparent: true,
+  map: pointMap
+})
 const points = new THREE.Points(geo, mat)
+const aroundPoints = new THREE.Points(aroundGeo, aroundMat)
 scene.add(points)
+scene.add(aroundPoints)
 const pos = geo.getAttribute('position')
 
-const boxGeo = new THREE.BoxGeometry(40, 40, 40, 10, 10, 10).getAttribute('position')
-const sphereGeo = new THREE.SphereGeometry(30, 30, 30).getAttribute('position')
+const boxGeo = new THREE.BoxGeometry(100, 100, 100, 10, 10, 10).getAttribute('position')
+const sphereGeo = new THREE.SphereGeometry(100, 20, 20).getAttribute('position')
 const maxCount = ref(0)
 
-const updateGeometry = (geometry: any, n: number) => {
-  const tweens = []
+const updateGeometry = (geometry: any, n: any) => {
   const { array, count } = geometry
   maxCount.value = maxCount.value > count ? maxCount.value : count
   for (let i = 0; i < maxCount.value; i += 1) {
     const newVertice = new THREE.Vector3(
-      geo.attributes.position.array[i * 3],
+      geo.attributes.position.array[i * 3] - n,
       geo.attributes.position.array[i * 3 + 1],
       geo.attributes.position.array[i * 3 + 2]
     )
 
-    tweens.push(new TWEEN.Tween(newVertice).easing(TWEEN.Easing.Exponential.In))
-    tweens[i]
+    new TWEEN.Tween(newVertice)
+      .easing(TWEEN.Easing.Exponential.In)
       .to(
         {
-          x: array[i * 3] * 2 + n,
-          y: array[i * 3 + 1] * 2,
-          z: array[i * 3 + 2] * 2
+          x: array[i * 3] === undefined ? Math.random() * 800 - 400 : array[i * 3] + n,
+          y:
+            array[i * 3 + 1] === undefined
+              ? Math.random() * 800 - 400
+              : array[i * 3 + 1],
+          z:
+            array[i * 3 + 2] === undefined
+              ? Math.random() * 800 - 400
+              : array[i * 3 + 2]
         },
-        Math.random() * 1000
+        1000
       )
-      .delay(Math.random() * 800)
+      .delay(Math.random() * 400)
       .onUpdate(() => {
-        pos.array[i * 3] = newVertice.x
+        pos.array[i * 3] = newVertice.x + n
         pos.array[i * 3 + 1] = newVertice.y
         pos.array[i * 3 + 2] = newVertice.z
         geo.setAttribute('position', new THREE.BufferAttribute(pos.array, 3))
         // pos.needsUpdate = true
       })
-
-    tweens[i].start()
-  }
-}
-
-const update = () => {
-  const arr = pos.array
-  for (let i = 0, s = MAX, j = 0; i < s; i += 1, j += 3) {
-    arr[j] = (Math.random() - 0.5) * 2000
-    arr[j + 1] = (Math.random() - 0.5) * 2000
-    arr[j + 2] = (Math.random() - 0.5) * 2000
+      .start()
   }
 }
 
 const animation = () => {
   controls.update()
+  aroundPoints.rotateX(Math.PI / 1000)
   TWEEN.update()
+  camera.lookAt(scene.position)
   renderer.render(scene, camera)
 }
 onMounted(async () => {
@@ -104,7 +119,6 @@ onMounted(async () => {
     updateSize(canvasRef.value, camera, renderer)
     canvasRef.value?.appendChild(renderer.domElement)
   }
-  update()
   renderer.setAnimationLoop(animation)
 })
 </script>
