72730076c3ba4d21e65e2e2831a26d75db5f105a
474288926 <474288926@users.noreply.github.com>
2024-02-06 14:36:23 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 7bb1a9c3d2186b562fe06edc811bba5769231820

--- particleEffects--ohaf_3G.js
+++ particleEffects--ohaf_3G.js
@@ -1,4 +1,4 @@
-import{d as t,r as e,o as n,x as i,c as r,e as a,f as s}from"./index-xrVANN2z.js";
+import{d as t,r as e,o as n,x as i,c as r,e as a,f as s}from"./index-qt47kGxj.js";
 /**
  * @license
  * Copyright 2010-2023 Three.js Authors
